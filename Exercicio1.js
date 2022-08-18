2814ca000f483e2ec73f04d28c75a9b405451e67
Saulo Michielin <saulomichielin@gmail.com>
2022-08-18 16:17:28 -0300

    Exercício 1, calculadora

---
+++ Exercicio1.js
@@ -0,0 +1,13 @@
+const a = 8;
+const b = 5;
+let soma = a + b;
+let subtração = a - b;
+let multiplicação = a * b;
+let divisão = a / b;
+let resto = a % b;
+
+console.log('O resultado da soma é: ' + soma);
+console.log('O resultado da subtração é: ' + subtração);
+console.log('O resultado da multiplicação é: ' + multiplicação);
+console.log('O resultado da divisão é: ' + divisão);
+console.log('O resultado do resto é: ' + resto);
