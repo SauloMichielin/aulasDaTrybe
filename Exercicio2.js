25a7b6a13cbce62d7bc757bce7cc0cdc3831f39c
Saulo Michielin <saulomichielin@gmail.com>
2022-08-18 20:01:50 -0300

    Adicionado novos exercícios

---
+++ Exercicio2.js
@@ -0,0 +1,9 @@
+const a = 8;
+const b = 15;
+
+if (a > b) {
+    console.log('O maior número é: ' + a);
+}
+else{
+    console.log('O maior número é: ' + b);
+}
---
+++ Exercicio3.js
@@ -0,0 +1,13 @@
+const a = 11
+const b = 9
+const c = 7
+let resultado
+
+if (a >= b){
+    resultado = a;
+} else if (b >= c){
+    resultado = b;
+} else {
+    resultado = c;
+}
+console.log(resultado);
