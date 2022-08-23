54e0ba6057956494d98d1c14c2a8a73d269f87c4
Saulo Michielin <saulomichielin@gmail.com>
2022-08-23 17:28:13 -0300

    Início das atividades

--- Exercício1.js
+++ Exercício1.js
@@ -1,54 +1,26 @@
-//Exercício 1 - Fatorial
-let fatorial = 1;
-for (let index = 1; index <= 5; index++){
-    fatorial *= index;
-}
-
-console.log('A soma fatorial de 5 é: '+fatorial); 
-
-//Inverter a palavra banana
-let palavra = 'Banana';
-let palavraInvertida = ''
-for (let index = palavra.length -1; index >= 0; index--){
-    palavraInvertida += palavra[index];
-}
-console.log('A palavra invertida de '+palavra + ' é '+palavraInvertida); //? 
-
-//Retorna a maior palavra de um array
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maiorPalavra = ''
-for (let index = 0; index < array.length; index++){
-    if (array[index].length > maiorPalavra.length){
-        maiorPalavra = array[index];
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+    
+    let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente:  'Sim'
     }
-}
-console.log('A maior palavra do array é: '+maiorPalavra);
-
-//Retorna a menor palavra de um array
-let menorPalavra = array[0];
-for (let index = 0; index < array.length; index++){
-    if (array[index].length < menorPalavra.length){
-        menorPalavra = array[index];
+    let cont = 0;
+    info['recorrente'] = 'Sim';
+    for (let index in info){
+        console.log(index);
+        console.log(info[index]);
+        cont += 1;
     }
-}
-console.log('A menor palavra do array é: '+menorPalavra);
-
-//Retorna o maior número primo menor que 50
-
-let primos = [];
-console.log(primos)
-let resultadoFinal = 0
-let temp = []
-for (let index = 2; index <= 50; index += 1){
-    temp.splice(0,temp.length)
-    for (let cont = 2; cont<index; cont +=1){        
-        if (index%cont == 0){
-            temp.push(0)
-            break            
-        }        
-    }    
-    if (temp.length === 0){
-        primos.push(index)
-    }
-}
-console.log(primos)
+    
+    console.log(info['personagem'] + ' e ' + info2['personagem']);
+    console.log(info['origem'] + ' e ' + info2['origem']);
+    console.log(info['nota'] + ' e ' + info2['nota']);
+    
+  
+    console.log('Bem vinda '+info['personagem']);
