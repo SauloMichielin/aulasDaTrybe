ef5da6677a0e63fec5886aec252954e4f2658b10
Saulo Michielin <saulomichielin@gmail.com>
2022-09-28 11:07:16 -0300

    só pra salvar

--- script.js
+++ script.js
@@ -10,77 +10,106 @@
 
 const estudantes = [
     {
-      nome: 'Jorge',
-      sobrenome: 'Silva',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 67 },
-        { name: 'Português', nota: 79 },
-        { name: 'Química', nota: 70 },
-        { name: 'Biologia', nota: 65 },
-      ],
+        nome: 'Jorge',
+        sobrenome: 'Silva',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 67 },
+            { name: 'Português', nota: 79 },
+            { name: 'Química', nota: 70 },
+            { name: 'Biologia', nota: 65 },
+        ],
     },
     {
-      nome: 'Mario',
-      sobrenome: 'Ferreira',
-      idade: 15,
-      turno: 'Tarde',
-      materias: [
-        { name: 'Matemática', nota: 59 },
-        { name: 'Português', nota: 80 },
-        { name: 'Química', nota: 78 },
-        { name: 'Biologia', nota: 92 },
-      ],
+        nome: 'Mario',
+        sobrenome: 'Ferreira',
+        idade: 15,
+        turno: 'Tarde',
+        materias: [
+            { name: 'Matemática', nota: 59 },
+            { name: 'Português', nota: 80 },
+            { name: 'Química', nota: 78 },
+            { name: 'Biologia', nota: 92 },
+        ],
     },
     {
-      nome: 'Jorge',
-      sobrenome: 'Santos',
-      idade: 15,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 76 },
-        { name: 'Português', nota: 90 },
-        { name: 'Química', nota: 70 },
-        { name: 'Biologia', nota: 80 },
-      ],
+        nome: 'Jorge',
+        sobrenome: 'Santos',
+        idade: 15,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 76 },
+            { name: 'Português', nota: 90 },
+            { name: 'Química', nota: 70 },
+            { name: 'Biologia', nota: 80 },
+        ],
     },
     {
-      nome: 'Maria',
-      sobrenome: 'Silveira',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 91 },
-        { name: 'Português', nota: 85 },
-        { name: 'Química', nota: 92 },
-        { name: 'Biologia', nota: 90 },
-      ],
+        nome: 'Maria',
+        sobrenome: 'Silveira',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 91 },
+            { name: 'Português', nota: 85 },
+            { name: 'Química', nota: 92 },
+            { name: 'Biologia', nota: 90 },
+        ],
     },
     {
-      nome: 'Natalia',
-      sobrenome: 'Castro',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 70 },
-        { name: 'Português', nota: 70 },
-        { name: 'Química', nota: 60 },
-        { name: 'Biologia', nota: 50 },
-      ],
+        nome: 'Natalia',
+        sobrenome: 'Castro',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 70 },
+            { name: 'Português', nota: 70 },
+            { name: 'Química', nota: 60 },
+            { name: 'Biologia', nota: 50 },
+        ],
     },
     {
-      nome: 'Wilson',
-      sobrenome: 'Martins',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 80 },
-        { name: 'Português', nota: 82 },
-        { name: 'Química', nota: 79 },
-        { name: 'Biologia', nota: 75 },
-      ],
+        nome: 'Wilson',
+        sobrenome: 'Martins',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 80 },
+            { name: 'Português', nota: 82 },
+            { name: 'Química', nota: 79 },
+            { name: 'Biologia', nota: 75 },
+        ],
     },
-  ];
-  
-  
+];
+
+const getBestClass = (acc, materia) => {
+    if (acc.nota > materia.nota) return acc;
+    return materia;
+};
+
+const reportBetter = (students) => students.map((student) => ({
+    name: student.nome,
+    materia: student.materias.reduce(getBestClass).name,
+}));
+
+console.log(reportBetter(estudantes));
+
+function numPrimos(numero) {
+    let primos = [1,2]
+    for (let i = 1; i <= numero; i += 1) {
+        let teste = 0;
+        if (i % 2 !== 0) {
+            for (let a = 1; a <= i; a += 1) {
+                if (i % a === 0) {
+                    teste++
+                }
+            }
+            if (teste === 2) {
+                primos.push(i)
+            }
+        }
+    }
+    return primos
+}
+console.log(numPrimos(1000));
